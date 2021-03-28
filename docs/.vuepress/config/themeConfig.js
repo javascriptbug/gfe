b2813d4375900862949c7a2c8665f045abba8620
javascriptbug <javascriptbug@126.com>
2021-03-28 23:16:56 +0800

    antdv css引入

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -47,8 +47,8 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'duanning', // 必需
+    link: 'null', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏

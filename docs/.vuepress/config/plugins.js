56b3e44a2650755e6135ad9d1216647eac72eca1
javascriptbug <javascriptbug@126.com>
2021-03-30 11:10:13 +0800

    无内容显示问题待解决

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -6,7 +6,7 @@ module.exports = [
   //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
   // }],
 
-  'vuepress-plugin-baidu-autopush', // 百度自动推送
+  // 'vuepress-plugin-baidu-autopush', // 百度自动推送
   'demo-container',
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
   [
@@ -73,31 +73,31 @@ module.exports = [
     },
   ],
   [
-    'vuepress-plugin-baidu-tongji', // 百度统计
-    {
-      hm: '503f098e7e5b3a5b5d8c5fc2938af002',
-    },
+    // 'vuepress-plugin-baidu-tongji', // 百度统计
+    // {
+    //   hm: '503f098e7e5b3a5b5d8c5fc2938af002',
+    // },
   ],
-  [
-    'vuepress-plugin-comment', // 评论
-    {
-      choosen: 'gitalk',
-      options: {
-        clientID: 'a6e1355287947096b88b',
-        clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-        repo: 'blog-gitalk-comment', // GitHub 仓库
-        owner: 'javascriptbug', // GitHub仓库所有者
-        admin: ['javascriptbug'], // 对仓库有写权限的人
-        // distractionFreeMode: true,
-        pagerDirection: 'last', // 'first'正序 | 'last'倒序
-        id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
-        title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
-        labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
-        body:
-          '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
-      },
-    },
-  ],
+  // [
+  //   'vuepress-plugin-comment', // 评论
+  //   {
+  //     choosen: 'gitalk',
+  //     options: {
+  //       clientID: 'a6e1355287947096b88b',
+  //       clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
+  //       repo: 'blog-gitalk-comment', // GitHub 仓库
+  //       owner: 'javascriptbug', // GitHub仓库所有者
+  //       admin: ['javascriptbug'], // 对仓库有写权限的人
+  //       // distractionFreeMode: true,
+  //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
+  //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
+  //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
+  //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
+  //       body:
+  //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+  //     },
+  //   },
+  // ],
   [
     '@vuepress/last-updated', // "上次更新"时间格式
     {

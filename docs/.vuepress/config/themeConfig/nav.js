59c97484073a7dd29ec66b23b02cb8b44fa9f9fa
javascriptbug <javascriptbug@126.com>
2021-03-30 09:55:43 +0800

    组件频道

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,6 +2,24 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
+    text: 'gui框架',
+    link: '/gui/antdv-components',
+    items: [
+      {
+        text: '关于',
+        items: [
+          { text: 'gui', link: '/pages/9002ed/' },
+        ],
+      },
+      {
+        text: '组件库',
+        items: [
+          { text: '组件一览', link: '/gui/antdv-componentss/' },
+        ],
+      },
+    ]
+  },
+  {
     text: '前端',
     link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
